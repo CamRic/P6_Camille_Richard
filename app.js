08721fbb5f8f2538e72b462357207857ab1e279f
camric <ymontantlointain@gmail.com>
2022-02-22 13:04:43 +0100

    etape 2

--- app.js
+++ app.js
@@ -1,25 +1,32 @@
 const express = require('express')
+const mongoose = require('mongoose')
+
+const userRoutes = require('./routes/user')
 
 const app = express()
+
+mongoose.connect('mongodb+srv://adminPiiquante:cLCJzzPprIEVzIZ2@piiquante.lkwp4.mongodb.net/OC_P6?retryWrites=true&w=majority',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => console.log('Connected to database'))
+    .catch(() => console.log('ERROR: can\'t connect to database'))
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+})
+
+app.use(express.json())
 
 app.use((req, res, next) => {
     console.log('requete recue')
     next()
 })
 
-app.use((req, res, next) => {
-    res.status(201)
-    next()
-})
-
-app.use((req, res, next) => {
-    res.json({message: '3...'})
-    next()
-})
-
-app.use((req, res, next) => {
-    console.log('fin taches')
-    //next()
-})
+app.use('/api/auth', userRoutes)
 
 module.exports = app
---
+++ controllers/user.js
@@ -0,0 +1,48 @@
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+
+const User = require('../models/User')
+
+exports.signup = (req, res, next) => {
+    // hashage du mdp
+    bcrypt.hash(req.body.password, 10)
+        .then(hash => {
+            const newUser = new User({
+                email: req.body.email,
+                password: hash
+            })
+            // enregistre utilisateur dans bdd
+            newUser.save()
+                .then(() => res.status(201).json({message: 'User Created'}))
+                .catch(error => res.status(400).json({ error: 'erreur bad request' }))
+        })
+        .catch(error => res.status(500).json({ error: "erreur serveur"}))
+}
+
+exports.login = (req, res, next) => {
+    // recherche de l'utilisateur correspondant au mail renseigné
+    User.findOne({email: req.body.email})
+        .then(user => {
+            if (!user) { // si entree email non valide
+                return res.status(401).json({error: 'Email invalide'})
+            }
+            // si email valide, verification du mot de passe
+            bcrypt.compare(req.body.password, user.password)
+                .then(valid => { 
+                    if (!valid) { // si mot de passe invalide
+                        return res.status(401).json({ error: 'Mot de passe non valide'})
+                    }
+                    const userToken = jwt.sign(
+                        {userId: user._id},
+                        'RANDOM_TOKEN_SECRET',
+                        {expiresIn: '24h'}
+                    )
+                    res.status(200).json({
+                        userId: user._id,
+                        token: userToken
+                    })
+                })
+                .catch(error => res.status(500).json({ error }))
+        })
+        .catch(error => res.status(500).json({ error }))
+}
